afc1b610d7ee55726b6c39d21d05173197b1b8cf
thezeeshanasghar <thezeeshanasghar@gmail.com>
2020-11-20 22:26:43 +0500

    update backend URL and create basic Readme file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,9 @@ export const environment = {
   //BASE_URL: "http://vac-api.ehs.edu.pk/api/",
   //BASE_URL: "https://api.vaccs.io/api/",
    //  BASE_URL: "http://localhost:5000/api/",
-  BASE_URL: "http://13.233.255.96:5002/api/",
+  // BASE_URL: "http://13.233.255.96:5002/api/",
 
+  BASE_URL: "https://be.vaccineapi.fernflowers.com/api/",
   // BASE_URL: "http://localhost:4309/api/"
 
   IS_LOGGED_IN: "IsLoggedIn"
