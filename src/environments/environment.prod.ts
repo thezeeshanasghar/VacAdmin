39a94dd474712fcb0f5eddbc62644d4b0e771851
alikhan00000 <106005013+alikhan00000@users.noreply.github.com>
2023-10-20 03:56:06 -0700

    changing api urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,8 +7,8 @@ export const environment = {
  //  BASE_URL: "http://localhost:5000/api/",
 
   // for using web server
-  BASE_URL: "https://fernflowers.com/api/",
-  RESOURCE_URL: "https://fernflowers.com/",
+  BASE_URL: "https://skintechno.com/api/",
+  RESOURCE_URL: "https://skintechno.com/",
 
  //  BASE_URL: "http://localhost:4309/api/",
 
